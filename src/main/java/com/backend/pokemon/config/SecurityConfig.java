e97f38a0c99d41f89b4b9e1e81b4bf2dbcb78a4b
AlexLaFuente02 <alexlafuentelp@gmail.com>
2023-11-16 18:40:27 -0400

    SecutiryOFF_AlmacenSUB

--- src/main/java/com/backend/pokemon/config/SecurityConfig.java
+++ src/main/java/com/backend/pokemon/config/SecurityConfig.java
@@ -1,11 +1,10 @@
 package com.backend.pokemon.config;
-/*
+/* 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -13,16 +12,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/").permitAll();
-                    auth.requestMatchers("/favicon.ico").permitAll();
-                    auth.anyRequest().authenticated();
-                })
-                .oauth2Login(withDefaults())
-                .formLogin(withDefaults())
-                .build();
-    }
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            // ... otras configuraciones de seguridad ...
+            .authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/users/createorlogin").permitAll() // Cambio aquí
+                .anyRequest().authenticated())
+            .httpBasic(withDefaults()); // Asegúrate de configurar la autenticación básica o JWT como necesites
 
-}*/
+        // Configuración CORS si es necesaria aquí
+        http.cors(withDefaults());
+
+
+
+        return http.build();
+    }
+} */
